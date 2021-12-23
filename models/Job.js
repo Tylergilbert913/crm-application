081967ce3cd5e93600a0a4f2a86648368ad96dcf
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-12-23 16:01:36 -0500

    created the job model

--- models/Job.js
+++ models/Job.js
@@ -1,6 +1,38 @@
 const { Model, DataTypes } = require("sequelize");
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
+class Job extends Model {}
 
-class Client extends Model { };
+Invoice.init(
+  {
+    JobID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tech_notes: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    ClientID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "client",
+        key: "ClientID",
+        unique: true,
+      },
+    },
+  },
 
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "job",
+  }
+);
+
+module.exports = Job;
