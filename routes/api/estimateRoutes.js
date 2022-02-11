fa22bd17a26822a0851a2bcb308299711defae8d
Tyler Gilbert <tyler.gilbert913@gmail.com>
2022-02-11 14:36:14 -0700

    created routes for estimateRoutes

--- routes/api/estimateRoutes.js
+++ routes/api/estimateRoutes.js
@@ -1,2 +1,18 @@
 const router = require('express').Router();
-const withAuth = require('../../utils/auth');
+const estimateController = require("../../estimateController");
+// const withAuth = require('../../utils/auth');
+
+
+// Matches with "/api/estimate" 
+router.route("/")
+.get(estimateController.findAll)
+.post(estimateController.create)
+
+// Matches with "/api/estimate/:id"
+router
+.route("/:id")
+.get(estimateController.findById)
+.put(estimateController.update)
+.delete(estimateController.remove);
+
+module.exports = router;
