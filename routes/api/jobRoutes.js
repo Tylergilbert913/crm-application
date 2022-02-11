5a4c1bc7b4111316df37ae3b374807b7d6d73bc2
Tyler Gilbert <tyler.gilbert913@gmail.com>
2022-02-11 14:42:57 -0700

    created routes for jobInvoice

--- routes/api/jobRoutes.js
+++ routes/api/jobRoutes.js
@@ -1,2 +1,17 @@
 const router = require('express').Router();
+const jobController = require("../../jobController");
 const withAuth = require('../../utils/auth');
+
+// Matches with "/api/job" 
+router.route("/")
+.get(jobController.findAll)
+.post(jobController.create)
+
+// Matches with "/api/job/:id"
+router
+.route("/:id")
+.get(jobController.findById)
+.put(jobController.update)
+.delete(jobController.remove);
+
+module.exports = router;
