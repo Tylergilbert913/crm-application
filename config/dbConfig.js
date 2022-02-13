4dfbd053a1dfd6429e3b1b5526b0b9a060eb1dbb
Tyler Gilbert <tyler.gilbert913@gmail.com>
2022-02-12 22:44:51 -0700

    creating a new config/dbConfig.js for mssql

---
+++ config/dbConfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+  HOST: "localhost",
+  PORT: "3001",
+  USER: "bradytyler",
+  PASSWORD: "jeepjetta1!",
+  DB: "crm_db",
+  dialect: "mssql",
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
--- server.js
+++ server.js
@@ -1,29 +1,22 @@
 const express = require("express");
-const session = require("express-session");
-// const path = require("path");
 const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+// const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const routes = require("./routes");
 const transporter = require("./config/nodemailer");
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+
+let corsOptions = {
+  origin: "https://localhost:3000",
+};
+
+app.use(cors(corsOptions));
 
 // Define Middleware for use on ALL paths starting with "/" on homepage
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Define and use session object here
-const sess = {
-  secret: "Super secret secret",
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
-};
-app.use(session(sess));
+db.sequelize.sync();
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -31,21 +24,20 @@ if (process.env.NODE_ENV === "production") {
 }
 
 //add routes
-app.use(routes);
+// app.use(routes);
 
-transporter.verify((err, success) => {
-  err
-    ? console.log(err)
-    : console.log(`=== Server is ready to take messages: ${success}`);
-});
-
-
-
-// Start the API Server
-sequelize.sync({ force: false }).then(() => {
+// transporter.verify((err, success) => {
+  //   err
+  //     ? console.log(err)
+  //     : console.log(`=== Server is ready to take messages: ${success}`);
+  // });
+  
+  const PORT = process.env.PORT || 3001;
+  
+  // Start the API Server
+  // drop the table if it already exists
+  db.sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
     console.log(`🌎 ===> API Server now listening on PORT ${PORT}!`)
   );
 });
-
-
