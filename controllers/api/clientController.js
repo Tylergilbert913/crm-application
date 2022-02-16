5021906c1bef6caf72c3cb235ec71057338a3e30
Tyler Gilbert <tyler.gilbert913@gmail.com>
2022-02-15 22:05:32 -0700

    corrected spelling error in findAll

--- controllers/api/clientController.js
+++ controllers/api/clientController.js
@@ -44,7 +44,7 @@ exports.findAll = (req, res) => {
     })
     .catch(err => {
         res.status(500).send({
-            message: reqerr.message || "An error occured while retrieving clients."
+            message: err.message || "An error occured while retrieving clients."
         })
     })
 };
