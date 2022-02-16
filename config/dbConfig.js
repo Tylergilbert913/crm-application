2292c4b51cf8c405e60470a319bcff87e24f07e9
Tyler Gilbert <tyler.gilbert913@gmail.com>
2022-02-15 20:28:38 -0700

    structuring the clientController

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -13,23 +13,6 @@ module.exports = {
   },
 };
 
-// another structured feature to fetch data with query
+// server name used for MSSQL database
+// server: "crmtexas.database.windows.net",
 
-// const Sequelize = require('sequelize');
-
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-//     dialect: 'mssql',
-//     host: process.env.DB_HOST, //This is an IP ADRESS
-//     dialectOptions: {
-//       options: {
-//         instanceName: process.env.DB_INSTANCE_NAME,
-//         trustServerCertificate: true
-//       },
-//     }
-//   }
-// );
-// module.exports = {
-//   sequelize,
-//   Sequelize
-// };
