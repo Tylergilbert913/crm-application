386088007069259af3c598dcce4f2759e82f9444
Tyler Gilbert <tyler.gilbert913@gmail.com>
2022-02-20 00:41:03 -0700

    created update route for jobController

--- controllers/api/jobController.js
+++ controllers/api/jobController.js
@@ -44,11 +44,45 @@ Client.findAll({ where: condition })
 };
 
 exports.findOne = (req, res) => {
-
+// Finding Job by their ID
+const id = req.params.id;
+Job.findByPk(id).then((data) => {
+  if (data) {
+    res.send(data);
+  } else {
+    res.status(404).send({
+      message: `Cannot find Job with id=${id}.`,
+    });
+  }
+  res.status(500).send({
+    message: "An error occured when retrieving Job with id=" + id,
+  });
+});
 };
 
 exports.update = (req, res) => {
-
+// Updating Jobs information by using their ID
+const id = req.params.id;
+Job.update(req.body, {
+  where: { id: id },
+})
+  .then((num) => {
+    if (num == 1) {
+      res.send({
+        message: "Job was updated succesfully.",
+      });
+    } else {
+      res.send({
+        message: `Cannot update Job with id=${id}.`,
+      });
+    }
+  })
+  .catch((err) => {
+    res.status(500).send({
+      message:
+        err.message || "An error occured when updating Job with id=" + id,
+    });
+  });
 };
 
 exports.delete = (req, res) => {
