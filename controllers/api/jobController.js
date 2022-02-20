30cb3b7470715c59896717fab84ee35b0fb6d236
Tyler Gilbert <tyler.gilbert913@gmail.com>
2022-02-20 00:20:19 -0700

    structured the create route for jobController

--- controllers/api/jobController.js
+++ controllers/api/jobController.js
@@ -3,7 +3,29 @@ const Job = db.job;
 const Op = db.Sequelize.Op;
 
 exports.create = (req, res) => {
+// Validating request
+if (!req.body.ClientID) {
+    res.status(400).send({
+      message: "Content can not be empty!",
+    });
+    return;
+  }
 
+  // Create a Job
+  const job = {
+    tech_notes: req.body.tech_notes,
+    ClientID: req.body.ClientID
+  };
+  // Save Job in the database
+  Job.create(job)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "An error occured while creating the Job.",
+      });
+    });
 };
 
 exports.findAll = (req, res) => {
