45e847157146980b6d36cc6703d0d913d0b3cc6f
Tyler Gilbert <tyler.gilbert913@gmail.com>
2022-03-09 23:43:54 -0700

    changed if conditional to succesfully update of num == num

--- controllers/estimateController.js
+++ controllers/estimateController.js
@@ -76,7 +76,7 @@ exports.update = (req, res) => {
     where: { id: id },
   })
     .then((num) => {
-      if (num == 1) {
+      if (num == num) {
         res.send({
           message: "Estimate was updated succesfully.",
         });
