688482f5a6a147e9ef18135163e59654b6ea2f80
Tyler Gilbert <tyler.gilbert913@gmail.com>
2022-04-05 13:58:02 -0600

    creating pages for CRM app

---
+++ client/src/pages/ClientPage/clientPage.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import API from "../utils/API.js";
+
+
+
+
+function ClientPage() {
+
+
+
+
+
+
+
+};
+
+export default ClientPage;
---
+++ client/src/pages/Clients/clients.js
@@ -0,0 +1,6 @@
+import React, { useEffect, useState } from "react";
+import API from "../utils/API";
+
+function Clients() {}
+
+export default Clients;
